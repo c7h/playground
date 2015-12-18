604c21a9da68291b8dc3ccda3a6ec20b0c591ab2
Christoph Gerneth <login@gerneth.info>
2015-12-18 12:48:03 +0100

    minor fixes in HTTPServerV4

--- caesar_ipv6_v4-httpd.py
+++ caesar_ipv6_v4-httpd.py
@@ -6,11 +6,12 @@ from caesar import CaesarCipher
 
 class CaesarHandler(SimpleHTTPRequestHandler):
   def do_GET(self):
+    # import pdb; pdb.set_trace()
     if self.path == '/caesar/encrypt':
       self.send_response(200)
-      self.send_header('Content-type', 'text/html')
+      self.send_header('Content-type', 'application/json')
       self.end_headers()
-      self.wfile.write("")
+      self.wfile.write("{}")
       return
     elif self.path == '/caesar/decrypt':
       self.send_response(200)
@@ -27,4 +28,12 @@ class HTTPServerV6(HTTPServer):
   address_family = socket.AF_INET6
 
 class HTTPServerV4(HTTPServer):
-  address_family = socket.AF_INET4
+  address_family = socket.AF_INET
+
+if __name__ == '__main__':
+  serverV6 = HTTPServerV6(('::', 8000), CaesarHandler)
+  serverV4 = HTTPServerV4(("0.0.0.0", 8001), CaesarHandler)
+  
+  # cool kids provide v6
+  serverV6.serve_forever()
+  serverV4.serve_forever()
